3b4a9c28373d159e72a8eb7a3294501de62802c5
duynguyenfpt <39914280+duynguyenfpt@users.noreply.github.com>
2022-01-26 11:50:03 +0700

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -4,7 +4,7 @@ from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 
 def print_hello():
-    return 'Hello world from first Airflow DAG! Update from Jake'
+    return 'Hello world from first Airflow DAG! Update from Jake 2'
 
 dag = DAG('hello_world_test_sync', description='Hello World DAG',
           schedule_interval='0 12 * * *',
